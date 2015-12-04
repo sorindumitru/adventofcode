aa8d2b4a9889bee753ca8f8283795b91eb474b1b
Sorin Dumitru <sdumitru@ixiacom.com>
2015-12-04 11:08:31 +0200

    day3: part 2

--- day3/Main.hs
+++ day3/Main.hs
@@ -1,6 +1,27 @@
-import Control.Monad
+import Data.Char (isSpace)
+import Data.List
+
+rstrip = reverse . dropWhile isSpace . reverse
 
 main :: IO ()
 main = do
     input <- getContents
+    print $ countHouses $ visitedHouses input
+    let indexedInput = zip [0..] input
+    let santaHouses = visitedHouses [ x | (index, x) <- indexedInput, even index]
+    let roboHouses = visitedHouses [ x | (index, x) <- indexedInput, odd index]
+    print $ countHouses (santaHouses ++ roboHouses)
     return ()
+
+visitedHouses input = getHouses $ rstrip input
+
+countHouses houses = length $ group $ sort houses
+
+startState = (0,0)
+
+getHouses input = scanl moveSanta startState input
+
+moveSanta (x, y) '^' = (x, y - 1)
+moveSanta (x, y) '>' = (x + 1, y)
+moveSanta (x, y) 'v' = (x, y + 1)
+moveSanta (x, y) '<' = (x - 1, y)
