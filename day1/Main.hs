ae5a696505a5e1ab1d2a8714b299d861e75b0fa0
Sorin Dumitru <sorin@returnze.ro>
2015-12-05 10:20:16 +0200

    upate day1

--- day1/Main.hs
+++ day1/Main.hs
@@ -1,15 +1,15 @@
 main :: IO ()
 main = do
 	input <- getContents
-	putStrLn $ getFloor input
-	putStrLn $ getBasementPos input
+	putStrLn $ show $ getFloor input
+	putStrLn $ show $ getBasementPos input
 	return ()
 
 getModifier '(' = 1
 getModifier ')' = -1
 getModifier _ = 0
 
-getFloor input = show $ sum $ map getModifier input
+getFloor input = sum $ map getModifier input
 
-getBasementPos input = show $ (head $ [index | (index, e) <- zip [0..] sums, e < 0]) + 1
+getBasementPos input = (head $ [index | (index, e) <- zip [0..] sums, e < 0]) + 1
 	where sums = scanl1 (+) (map getModifier input)
